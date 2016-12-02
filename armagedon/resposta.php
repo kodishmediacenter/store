6d63e3195ad97bb68c855509e4696d0395c45070
kodishmediacenter <fabiolmglgeo@gmail.com>
2016-12-01 22:36:39 -0200

    fix

---
+++ armagedon/resposta.php
@@ -0,0 +1,5 @@
+<?php
+ $busca = $_POST['busca'];
+ #echo "<a href='http://www.armagedomfilmes.biz/?s=$busca'>Ver Resultado </a>";
+ header("Location: http://www.armagedomfilmes.biz/?s=$busca");
+ ?>
