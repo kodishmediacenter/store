96847ed820ab2bd3d2624025832fee4dd6dcca29
kodishmediacenter <fabiolmglgeo@gmail.com>
2017-08-27 19:02:40 -0300

    4.2
    Atualização do Kodi 16.1 para kodi 17.3

---
+++ Kodish SO/4.2/kodishupdate-4.2.sh
@@ -0,0 +1,9 @@
+sudo apt-get remove python 
+
+sudo apt-get install python-pip
+sudo -H pip uninstall pyOpenSSL
+sudo apt-get install kodi
+sudo apt-get install playonlinux
+sudo apt-get kodi-pvr-iptvsimple
+sudo apt autoremove
+
