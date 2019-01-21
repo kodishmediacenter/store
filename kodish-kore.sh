a729dd27000de9c00c6323f8b55ac3d7dadcd257
kodishmediacenter <fabiolmglgeo@gmail.com>
2019-01-21 10:31:29 -0200

    Update kodish-kore.sh

--- kodish-kore.sh
+++ kodish-kore.sh
@@ -1,20 +1,20 @@
 sudo add-apt-repository ppa:team-xbmc/ppa
 sudo apt-get update
-sudo apt-get install kodi
-sudo apt-get install kodi-audiodecoder-*
-sudo apt-get install kodi-screensavers-rsxs
-sudo apt-get install kodi-visualization-goom	
-sudo apt-get install kodi-visualization-projectm	
-sudo apt-get install kodi-visualization-shadertoy	
-sudo apt-get install kodi-visualization-spectrum	
-sudo apt-get install kodi-visualization-waveform	
-sudo apt-get install xbmc-visualization-fishbmc
-sudo apt-get install kodi-pvr-iptvsimple
+sudo apt-get install kodi -y
+sudo apt-get install kodi-audiodecoder-* -y
+sudo apt-get install kodi-screensavers-rsxs -y
+sudo apt-get install kodi-visualization-goom -y	
+sudo apt-get install kodi-visualization-projectm	-y
+sudo apt-get install kodi-visualization-shadertoy	-y
+sudo apt-get install kodi-visualization-spectrum	 -y
+sudo apt-get install kodi-visualization-waveform	-y
+sudo apt-get install xbmc-visualization-fishbmc -y
+sudo apt-get install kodi-pvr-iptvsimple -y
 sudo add-apt-repository ppa:stebbins/handbrake-releases
 sudo apt-get update
-apt-get install handbrake-gtk
+apt-get install handbrake-gtk -y
 sudo snap install spotify
-sudo apt-get install asunder
+sudo apt-get install asunder -y
 mkdir /opt/kodish
 chmod 7777 /opt/kodish
 cd /opt/kodish
