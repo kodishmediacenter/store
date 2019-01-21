0ffbb8b41f6cb4d48ce94cd670cbf5ca4ae67c9d
kodishmediacenter <fabiolmglgeo@gmail.com>
2019-01-21 10:20:34 -0200

    Create kodish-kore.sh

---
+++ kodish-kore.sh
@@ -0,0 +1,21 @@
+sudo add-apt-repository ppa:team-xbmc/ppa
+sudo apt-get update
+sudo apt-get install kodi
+sudo apt-get install kodi-audiodecoder-*
+sudo apt-get install kodi-screensavers-rsxs
+sudo apt-get install kodi-visualization-goom	
+sudo apt-get install kodi-visualization-projectm	
+sudo apt-get install kodi-visualization-shadertoy	
+sudo apt-get install kodi-visualization-spectrum	
+sudo apt-get install kodi-visualization-waveform	
+sudo apt-get install xbmc-visualization-fishbmc
+sudo apt-get install kodi-pvr-iptvsimple
+sudo add-apt-repository ppa:stebbins/handbrake-releases
+sudo apt-get update
+apt-get install handbrake-gtk
+sudo snap install spotify
+sudo apt-get install asunder
+mkdir /opt/kodish
+chmod 7777 /opt/kodish
+cd /opt/kodish
+mkdir server
